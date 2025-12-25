bd6c21a84c0040053d54f157e95e4286def8e3e1
alfadormx <alfadormx@users.noreply.github.com>
2025-12-25 18:35:02 +0100

    feat(Headline): added new block

---
+++ src/types/headline.types.ts
@@ -0,0 +1,10 @@
+export interface HeadlineProps {
+  tagline?: string;
+  title: string;
+  subtitle?: string;
+  taglineClass?: string;
+  titleClass?: string;
+  subtitleClass?: string;
+  class?: string;
+  align?: 'left' | 'center' | 'right';
+}
