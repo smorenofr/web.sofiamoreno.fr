cc0ab1cd22355771fa63e75cba2de05f8cf395e7
alfadormx <alfadormx@users.noreply.github.com>
2026-01-02 18:39:48 +0100

    renaming(FAQ): to ItemsContent block

--- src/types/itemscontent.types.ts
+++ src/types/itemscontent.types.ts
@@ -2,7 +2,7 @@ import type { ContainerProps } from './container.types';
 import type { HeadlineProps } from './headline.types';
 import type { ItemsGridProps } from './itemsgrid.types';
 
-export interface FAQProps {
+export interface ItemsContentProps {
   container?: Partial<ContainerProps>;
   headline?: Partial<HeadlineProps>;
   itemsGrid?: Partial<ItemsGridProps>;
