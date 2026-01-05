67b710ce32244c92546ee20507f4fd7e7e43bc8c
alfadormx <alfadormx@users.noreply.github.com>
2026-01-05 23:34:45 +0100

    chore(Documentation): Added navigation component to layout

---
+++ src/pages/documentation/navigation.ts
@@ -0,0 +1,41 @@
+import type { ButtonProps } from '~/types/button.types';
+
+export const documentationNavigation: ButtonProps[] = [
+  {
+    label: 'Base',
+    children: [
+      { label: 'Colors', href: '/documentation/base/Colors' },
+      { label: 'Typography', href: '/documentation/base/Typography' },
+    ],
+  },
+  {
+    label: 'Composite',
+    children: [
+      { label: 'CallToAction', href: '/documentation/composite/CallToAction' },
+      { label: 'Content', href: '/documentation/composite/Content' },
+      { label: 'ItemsContent', href: '/documentation/composite/ItemsContent' },
+      { label: 'ItemsGrid', href: '/documentation/composite/ItemsGrid' },
+      { label: 'NavigationTree', href: '/documentation/composite/NavigationTree' },
+    ],
+  },
+  {
+    label: 'Layout',
+    children: [
+      { label: 'Container', href: '/documentation/layout/Container' },
+      { label: 'TwoColumnContainer', href: '/documentation/layout/TwoColumnContainer' },
+      { label: 'ThreeColumnContainer', href: '/documentation/layout/ThreeColumnContainer' },
+      { label: 'FourColumnContainer', href: '/documentation/layout/FourColumnContainer' },
+      { label: 'SidebarLeftContainer', href: '/documentation/layout/SidebarLeftContainer' },
+      { label: 'SidebarRightContainer', href: '/documentation/layout/SidebarRightContainer' },
+    ],
+  },
+  {
+    label: 'Primitives',
+    children: [
+      { label: 'Button', href: '/documentation/primitives/Button' },
+      { label: 'Headline', href: '/documentation/primitives/Headline' },
+      { label: 'Image', href: '/documentation/primitives/Image' },
+      { label: 'ThemeToggle', href: '/documentation/primitives/ThemeToggle' },
+    ],
+  },
+];
