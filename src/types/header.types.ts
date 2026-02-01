2111e22a27e8b83b99c5edc8be37c80d70f22467
alfadormx <alfadormx@users.noreply.github.com>
2026-02-01 01:19:32 +0100

    refacto(Header): header has been revisited to set Logo in the center and have lateral actions (left and right)

--- src/types/header.types.ts
+++ src/types/header.types.ts
@@ -5,20 +5,20 @@ import type { ButtonProps } from './button.types';
 import type { LogoProps } from './logo.types';
 
 export type HeaderBreakpoint = 'sm' | 'md' | 'lg';
-export type HeaderNavigationAlign = 'left' | 'center' | 'right';
 export type HeaderActionsAir = 'none' | 'tight' | 'normal' | 'loose';
 
 export interface HeaderProps extends Omit<HTMLAttributes<'nav'>, 'class'> {
   isSticky?: boolean;
   isFloating?: boolean;
   showThemeToggle?: boolean;
-  breakpoint?: HeaderBreakpoint;
-  navigationAlign?: HeaderNavigationAlign;
   actionsAir?: HeaderActionsAir;
   container?: ContainerProps;
   logo?: LogoProps;
-  navigationTreeHorizontal?: NavigationTreeHorizontalProps;
+  navigationLeft?: NavigationTreeHorizontalProps;
+  navigationRight?: NavigationTreeHorizontalProps;
   themeToggle?: ButtonProps;
-  actions?: ButtonProps[];
+  actionsLeft?: ButtonProps[];
+  actionsRight?: ButtonProps[];
+  defaultActionsConfig?: Partial<ButtonProps>;
   class?: string;
 }
